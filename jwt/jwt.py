adc370ac94a8a00435a1429040ffb029d96b0baf
tspspi <11219971+tspspi@users.noreply.github.com>
2024-08-18 09:39:24 +0200

    Adding generic parsing functionality

---
+++ jwt/jwt.py
@@ -0,0 +1,48 @@
+import json
+import base64
+
+from jws import JWS
+from jwe import JWE
+
+# Generic wrapper around JWS and JWE to allow parsing both
+# automatically determining their typ. This is used internally
+# when deserializing ...
+
+def base64url_encode(data):
+    return base64.urlsafe_b64encode(data).rstrip(b'=').decode('utf-8')
+
+def base64url_decode(data):
+    padding = '=' * (4 - (len(data) % 4))
+    return base64.urlsafe_b64decode(data + padding)
+
+def parse_jwt(data, keystore = None):
+    if isinstance(data, str):
+        # Check if we are one of the compact serializations
+        parts = data.split(".")
+        if len(parts) == 3:
+            # This is most likely a JWS
+            return JWS.parse(data, keystore)
+        if len(parts) == 5:
+            # This is most likely a JWE
+            return JWE.parse(data, keystore)
+
+    if not isinstance(data, dict):
+        # We either have a compact serialization or 
+        # a JSON string. First try to split at dots
+        jdata = json.loads(data)
+    else:
+        jdata = data
+
+    if "protected" not in jdata:
+        # This is not a supported datatype
+        raise ValueError("Not a supported JWT")
+    phdr = json.loads(base64url_decode(jdata["protected"]))
+    if "enc" in phdr:
+        return JWE.parse(data, keystore)
+    elif ("typ" in phdr) and (phdr["typ"] == "JWT"):
+        return JWS.parse(data, keystore)
+
+    # ToDo: Can it also be a JWK?
+
+    raise ValueError("Not a supported JWT")
+        
